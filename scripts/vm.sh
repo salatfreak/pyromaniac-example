7e2fdb766bccb1f8100aaabb2aff1476aeac8028
dipsy <dipsy@local>
2026-08-23 14:06:45 +0200

    improve vm.sh consistency and make RAM variable

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -7,6 +7,7 @@ declare -ra TCP_PORTS=(19521:19521 10443:443 10080:80 10022:22)
 declare -ra UDP_PORTS=()
 readonly ROOT_DISK='./virtual-machine/root.qcow2'
 readonly ROOT_SIZE='40G'
+readonly RAM_SIZE='8G'
 
 # parse args until positional
 args="$(getopt -l install:,help -o i:h -- "$@")"
@@ -30,13 +31,13 @@ net="passt,model=virtio-net-pci"
 host_ipv4="$(ip route get 9.9.9.9 | sed -n 's/.* src \([^ ]\+\).*/\1/p')"
 if [[ -n "$host_ipv4" ]]; then
   vm_ipv4="${host_ipv4%.*}.$(( ${host_ipv4##*.} + 1 ))"
-  net+=",param=--address,param=$vm_ipv4,param=--map-host-loopback,param=$host_ipv4"
+  net+=",param=--address,param=${vm_ipv4},param=--map-host-loopback,param=${host_ipv4}"
 fi
 
 host_ipv6="$(ip -6 route get 2620:fe::fe | sed -n 's/.* src \([^ ]\+\).*/\1/p')"
 if [[ -n "$host_ipv6" ]]; then
   vm_ipv6="${host_ipv6%:*}:$(printf '%x' $(( 0x${host_ipv6##*:} + 1 )))"
-  net+=",param=--address,param=$vm_ipv6,param=--map-host-loopback,param=$host_ipv6"
+  net+=",param=--address,param=${vm_ipv6},param=--map-host-loopback,param=${host_ipv6}"
 fi
 
 if (( ${#TCP_PORTS[@]} > 0 ))
@@ -47,7 +48,7 @@ then net+=",udp-ports=$(printf '%s,,' "${UDP_PORTS[@]}")"; net="${net%,,}"; fi
 
 # assemble arguments and start virtual machine in TTY
 args=(
-  -name "FCOS" -enable-kvm -cpu host -m 8G -nographic -serial mon:stdio
+  -name "FCOS" -enable-kvm -cpu host -m "$RAM_SIZE" -nographic
   -drive "file=${ROOT_DISK},if=virtio" -nic "$net"
 )
 if [[ -n "$installer" ]]; then args+=(-cdrom "$installer" -boot 'order=c,once=d'); fi
